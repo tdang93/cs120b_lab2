c7cb516a3943bb130558ae5cf2e8f67db867eff6
LChan63 <43819537+LChan63@users.noreply.github.com>
2020-01-12 23:32:04 -0800

    Update lchan063_lab2_part1.c
    header change

--- Lab2_introToAVR2/turnin/lchan063_lab2_part1.c
+++ Lab2_introToAVR2/turnin/lchan063_lab2_part1.c
@@ -1,7 +1,7 @@
 /*	Author: lchan063
- *  Partner(s) Name: Tandy Dang
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *  Partner(s) Name: Tandy Dang (tdang018@ucr.edu)
+ *	Lab Section: 022
+ *	Assignment: Lab #2  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
